6caeb9f78761984a2d7273967d7f387e2b633ae7
blecking <jens.blecking@gmail.com>
2025-11-14 13:11:46 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ function add_active_state_event_listener() {
   for (var i = 0; i < nav_items.length; i++) {
     nav_items[i].addEventListener("click", function() {
       var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
+      current[0].className = current[0].className.replace("active", "");
       this.className += " active";
     });
   }
